f35001b54d897a825471fc4d1c84681bd63c35be
Leonardo Sousa <leonardolsousa5@gmail.com>
2021-09-16 19:15:54 -0300

    Inserindo script e nodemon

--- Chapter I/src/index.js
+++ Chapter I/src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (request, response) => {
-    return response.json({ message: "Hello Ignite!!!" })
+    return response.json({ message: "Hello Igniters!!!!!" })
 });
 
 app.listen(3333);
