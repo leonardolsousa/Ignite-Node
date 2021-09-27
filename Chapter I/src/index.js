73300fd188d6ae24740568e16a146796a74cf75a
Leonardo Sousa <leonardolsousa5@gmail.com>
2021-09-27 19:45:00 -0300

    Endpoint de débito

--- Chapter I/src/index.js
+++ Chapter I/src/index.js
@@ -30,6 +30,18 @@ function verifyIfExistsAccountCPF(request, response, next) {
     return next();
 }
 
+function getBalance(statement) {
+    const balance = statement.reduce((accumulate, operation) => {
+        if (operation.type === 'credit') {
+            return accumulate + operation.amount;
+        } else {
+            return accumulate - operation.amount;
+        }
+    }, 0);
+
+    return balance;
+}
+
 app.post("/account", (request, response) => {
 
     const { cpf, name } = request.body;
@@ -76,6 +88,31 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
     customer.statement.push(statementOperation);
 
     return response.status(201).send();
+
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+    const { amount } = request.body;
+
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if (balance < amount) {
+        return response.status(400).json({ error: "Insufficient funds!" })
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit",
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+
+
 
 });
 
